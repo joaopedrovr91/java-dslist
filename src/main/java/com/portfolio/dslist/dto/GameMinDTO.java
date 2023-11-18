a1ba0cab5124f56687bfcf7cb3a7fedd1550799d
joaopedrovr91 <joaopedrovr91@gmail.com>
2023-11-18 16:20:24 -0300

    deploy ready

--- src/main/java/com/portfolio/dslist/dto/GameMinDTO.java
+++ src/main/java/com/portfolio/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.portfolio.dslist.dto;
 
 import com.portfolio.dslist.entities.Game;
+import com.portfolio.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -10,13 +11,20 @@ public class GameMinDTO {
 	private String imgUrl;
 	private String shortDescription;
 
-
     public GameMinDTO(Game entity) {
         id = entity.getId();
         title = entity.getTitle();
         year = entity.getYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
