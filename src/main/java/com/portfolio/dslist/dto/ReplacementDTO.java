9089d9e11a8f6a211b6d5a922435613885f46f33
joaopedrovr91 <joaopedrovr91@gmail.com>
2023-11-19 15:30:42 -0300

    Move on list

---
+++ src/main/java/com/portfolio/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,26 @@
+package com.portfolio.dslist.dto;
+
+public class ReplacementDTO {
+
+    private Integer sourceIndex;
+    private Integer destinationIndex;
+    
+
+    public Integer getSourceIndex() {
+        return this.sourceIndex;
+    }
+
+    public void setSourceIndex(Integer sourceIndex) {
+        this.sourceIndex = sourceIndex;
+    }
+
+    public Integer getDestinationIndex() {
+        return this.destinationIndex;
+    }
+
+    public void setDestinationIndex(Integer destinationIndex) {
+        this.destinationIndex = destinationIndex;
+    }
+
+
+}
